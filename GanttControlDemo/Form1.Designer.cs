ddab4c7b03d3a7ff1df20905da43e39d32441c2e
IlqarN <ilqar.novruzov.z@asoiu.edu.az>
2024-06-20 23:34:27 +0400

    Add project files.

---
+++ GanttControlDemo/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace GanttControlDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainRibbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.bbiSave = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiSaveAndClose = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiSaveAndNew = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiReset = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiDelete = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiClose = new DevExpress.XtraBars.BarButtonItem();
+            this.btnCalc = new DevExpress.XtraBars.BarButtonItem();
+            this.mainRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.mainRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ganttControl1 = new DevExpress.XtraGantt.GanttControl();
+            ((System.ComponentModel.ISupportInitialize)(this.mainRibbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ganttControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainRibbonControl
+            // 
+            this.mainRibbonControl.EmptyAreaImageOptions.ImagePadding = new System.Windows.Forms.Padding(35, 37, 35, 37);
+            this.mainRibbonControl.ExpandCollapseItem.Id = 0;
+            this.mainRibbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.mainRibbonControl.ExpandCollapseItem,
+            this.mainRibbonControl.SearchEditItem,
+            this.bbiSave,
+            this.bbiSaveAndClose,
+            this.bbiSaveAndNew,
+            this.bbiReset,
+            this.bbiDelete,
+            this.bbiClose,
+            this.btnCalc});
+            this.mainRibbonControl.Location = new System.Drawing.Point(0, 0);
+            this.mainRibbonControl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mainRibbonControl.MaxItemId = 11;
+            this.mainRibbonControl.Name = "mainRibbonControl";
+            this.mainRibbonControl.OptionsMenuMinWidth = 385;
+            this.mainRibbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.mainRibbonPage});
+            this.mainRibbonControl.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+            this.mainRibbonControl.Size = new System.Drawing.Size(931, 193);
+            this.mainRibbonControl.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden;
+            // 
+            // bbiSave
+            // 
+            this.bbiSave.Caption = "Save";
+            this.bbiSave.Id = 2;
+            this.bbiSave.ImageOptions.ImageUri.Uri = "Save";
+            this.bbiSave.Name = "bbiSave";
+            // 
+            // bbiSaveAndClose
+            // 
+            this.bbiSaveAndClose.Caption = "Save And Close";
+            this.bbiSaveAndClose.Id = 3;
+            this.bbiSaveAndClose.ImageOptions.ImageUri.Uri = "SaveAndClose";
+            this.bbiSaveAndClose.Name = "bbiSaveAndClose";
+            // 
+            // bbiSaveAndNew
+            // 
+            this.bbiSaveAndNew.Caption = "Save And New";
+            this.bbiSaveAndNew.Id = 4;
+            this.bbiSaveAndNew.ImageOptions.ImageUri.Uri = "SaveAndNew";
+            this.bbiSaveAndNew.Name = "bbiSaveAndNew";
+            // 
+            // bbiReset
+            // 
+            this.bbiReset.Caption = "Reset Changes";
+            this.bbiReset.Id = 5;
+            this.bbiReset.ImageOptions.ImageUri.Uri = "Reset";
+            this.bbiReset.Name = "bbiReset";
+            this.bbiReset.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.bbiReset_ItemClick);
+            // 
+            // bbiDelete
+            // 
+            this.bbiDelete.Caption = "Delete";
+            this.bbiDelete.Id = 6;
+            this.bbiDelete.ImageOptions.ImageUri.Uri = "Delete";
+            this.bbiDelete.Name = "bbiDelete";
+            // 
+            // bbiClose
+            // 
+            this.bbiClose.Caption = "Close";
+            this.bbiClose.Id = 7;
+            this.bbiClose.ImageOptions.ImageUri.Uri = "Close";
+            this.bbiClose.Name = "bbiClose";
+            this.bbiClose.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.bbiClose_ItemClick);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Caption = "Calculate";
+            this.btnCalc.Id = 10;
+            this.btnCalc.ImageOptions.Image = global::GanttControlDemo.Properties.Resources.calculatesheet_16x16;
+            this.btnCalc.ImageOptions.LargeImage = global::GanttControlDemo.Properties.Resources.calculatesheet_32x32;
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnCalc_ItemClick);
+            // 
+            // mainRibbonPage
+            // 
+            this.mainRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.mainRibbonPageGroup});
+            this.mainRibbonPage.MergeOrder = 0;
+            this.mainRibbonPage.Name = "mainRibbonPage";
+            this.mainRibbonPage.Text = "Home";
+            // 
+            // mainRibbonPageGroup
+            // 
+            this.mainRibbonPageGroup.AllowTextClipping = false;
+            this.mainRibbonPageGroup.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.False;
+            this.mainRibbonPageGroup.ItemLinks.Add(this.btnCalc);
+            this.mainRibbonPageGroup.ItemLinks.Add(this.bbiReset);
+            this.mainRibbonPageGroup.ItemLinks.Add(this.bbiClose);
+            this.mainRibbonPageGroup.Name = "mainRibbonPageGroup";
+            this.mainRibbonPageGroup.Text = "Tasks";
+            // 
+            // ganttControl1
+            // 
+            this.ganttControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ganttControl1.Location = new System.Drawing.Point(0, 193);
+            this.ganttControl1.MenuManager = this.mainRibbonControl;
+            this.ganttControl1.Name = "ganttControl1";
+            this.ganttControl1.OptionsBehavior.UpdateDependentTaskProgress = DevExpress.Utils.DefaultBoolean.True;
+            this.ganttControl1.OptionsCustomization.AllowModifyDependencies = DevExpress.Utils.DefaultBoolean.True;
+            this.ganttControl1.OptionsCustomization.AllowModifyProgress = DevExpress.Utils.DefaultBoolean.True;
+            this.ganttControl1.OptionsCustomization.AllowModifyTasks = DevExpress.Utils.DefaultBoolean.True;
+            this.ganttControl1.OptionsSplitter.SplitterThickness = 6;
+            this.ganttControl1.OptionsView.ShowBaselines = true;
+            this.ganttControl1.OptionsView.ShowIndentAsRowStyle = true;
+            this.ganttControl1.OptionsView.ShowTreeLines = DevExpress.Utils.DefaultBoolean.True;
+            this.ganttControl1.Size = new System.Drawing.Size(931, 544);
+            this.ganttControl1.SplitterPosition = 400;
+            this.ganttControl1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(931, 737);
+            this.Controls.Add(this.ganttControl1);
+            this.Controls.Add(this.mainRibbonControl);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Ribbon = this.mainRibbonControl;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainRibbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ganttControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.Ribbon.RibbonControl mainRibbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonPage mainRibbonPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup mainRibbonPageGroup;
+        private DevExpress.XtraBars.BarButtonItem bbiSave;
+        private DevExpress.XtraBars.BarButtonItem bbiSaveAndClose;
+        private DevExpress.XtraBars.BarButtonItem bbiSaveAndNew;
+        private DevExpress.XtraBars.BarButtonItem bbiReset;
+        private DevExpress.XtraBars.BarButtonItem bbiDelete;
+        private DevExpress.XtraBars.BarButtonItem bbiClose;
+        private DevExpress.XtraGantt.GanttControl ganttControl1;
+        private DevExpress.XtraBars.BarButtonItem btnCalc;
+    }
+}
---
+++ GanttControlDemo/Form1.cs
@@ -0,0 +1,92 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraLayout;
+using DevExpress.XtraLayout.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GanttControlDemo
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public class GanttTask
+        {
+            public int TaskID { get; set; }
+            public int ParentID { get; set; }
+            public string TaskName { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public DateTime BaseStartDate { get; set; }
+            public DateTime BaseEndDate { get; set; }
+            public List<int> Dependencies { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ganttControl1.KeyFieldName = "TaskID";
+            ganttControl1.ParentFieldName = "ParentID";
+            ganttControl1.ChartMappings.TextFieldName = "TaskName";
+            ganttControl1.ChartMappings.StartDateFieldName = "StartDate";
+            ganttControl1.ChartMappings.FinishDateFieldName = "EndDate";
+            ganttControl1.ChartMappings.BaselineStartDateFieldName = "StartDate";
+            ganttControl1.ChartMappings.BaselineFinishDateFieldName = "EndDate";
+            ganttControl1.OptionsView.ShowBaselines = true;
+
+        }
+        private List<GanttTask> GetGanttTasks()
+        {
+            return new List<GanttTask>
+            {
+                new GanttTask { TaskID = 1, ParentID = 0, TaskName = "Project Start1", StartDate = DateTime.Now, EndDate = DateTime.Now.AddYears(3),BaseStartDate = DateTime.Now, BaseEndDate = DateTime.Now.AddYears(3)},
+                    new GanttTask { TaskID = 2, ParentID = 1, TaskName = "Task 1", StartDate = DateTime.Now.AddMonths(3), EndDate = DateTime.Now.AddMonths(6), BaseStartDate = DateTime.Now.AddMonths(4), BaseEndDate = DateTime.Now.AddMonths(7), Dependencies = new List<int>()  },
+                    new GanttTask { TaskID = 3, ParentID = 1, TaskName = "Task 2", StartDate = DateTime.Now.AddMonths(7), EndDate = DateTime.Now.AddMonths(10), BaseStartDate = DateTime.Now.AddMonths(8), BaseEndDate = DateTime.Now.AddMonths(11) , Dependencies = new List<int>() },
+                    new GanttTask { TaskID = 4, ParentID = 1, TaskName = "Task 3", StartDate = DateTime.Now.AddMonths(11), EndDate = DateTime.Now.AddMonths(14), BaseStartDate = DateTime.Now.AddMonths(12), BaseEndDate = DateTime.Now.AddMonths(15) , Dependencies = new List<int>() },
+                new GanttTask { TaskID = 5, ParentID = 0, TaskName = "Project Start2", StartDate = DateTime.Now, EndDate = DateTime.Now.AddYears(3), BaseStartDate = DateTime.Now, BaseEndDate = DateTime.Now.AddYears(3) },
+                    new GanttTask { TaskID = 6, ParentID = 5, TaskName = "Task 1", StartDate = DateTime.Now.AddMonths(1), EndDate = DateTime.Now.AddMonths(5), BaseStartDate = DateTime.Now.AddMonths(3), BaseEndDate = DateTime.Now.AddMonths(7) , Dependencies = new List<int>() },
+                    new GanttTask { TaskID = 7, ParentID = 5, TaskName = "Task 2", StartDate = DateTime.Now.AddMonths(6), EndDate = DateTime.Now.AddMonths(10), BaseStartDate = DateTime.Now.AddMonths(12), BaseEndDate = DateTime.Now.AddMonths(13) , Dependencies = new List<int>() },
+                    new GanttTask { TaskID = 8, ParentID = 5, TaskName = "Task 3", StartDate = DateTime.Now.AddMonths(11), EndDate = DateTime.Now.AddMonths(15), BaseStartDate = DateTime.Now.AddMonths(17), BaseEndDate = DateTime.Now.AddMonths(22) , Dependencies = new List<int>() },
+                new GanttTask { TaskID = 9, ParentID = 0, TaskName = "Project Start3", StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1), BaseStartDate = DateTime.Now, BaseEndDate = DateTime.Now.AddYears(2) },
+                    new GanttTask { TaskID = 10, ParentID = 9, TaskName = "Task 1", StartDate = DateTime.Now.AddMonths(2), EndDate = DateTime.Now.AddMonths(4), BaseStartDate = DateTime.Now.AddMonths(3), BaseEndDate = DateTime.Now.AddMonths(5) , Dependencies = new List<int>() },
+                    new GanttTask { TaskID = 11, ParentID = 9, TaskName = "Task 2", StartDate = DateTime.Now.AddMonths(5), EndDate = DateTime.Now.AddMonths(7), BaseStartDate = DateTime.Now.AddMonths(6), BaseEndDate = DateTime.Now.AddMonths(8) , Dependencies = new List<int>() },
+                    new GanttTask { TaskID = 12, ParentID = 9, TaskName = "Task 3", StartDate = DateTime.Now.AddMonths(8), EndDate = DateTime.Now.AddMonths(10), BaseStartDate = DateTime.Now.AddMonths(9), BaseEndDate = DateTime.Now.AddMonths(11), Dependencies = new List<int>() },
+                new GanttTask { TaskID = 13, ParentID = 0, TaskName = "Project Start4", StartDate = DateTime.Now, EndDate = DateTime.Now.AddYears(4), BaseStartDate = DateTime.Now, BaseEndDate = DateTime.Now.AddMonths(5) },
+                    new GanttTask { TaskID = 14, ParentID = 13, TaskName = "Task 1", StartDate = DateTime.Now, EndDate = DateTime.Now.AddYears(1), BaseStartDate = DateTime.Now.AddMonths(1), BaseEndDate = DateTime.Now.AddMonths(2) , Dependencies = new List<int>()},
+                    new GanttTask { TaskID = 15, ParentID = 13, TaskName = "Task 2", StartDate = DateTime.Now.AddYears(1), EndDate = DateTime.Now.AddYears(2), BaseStartDate = DateTime.Now.AddYears(2), BaseEndDate = DateTime.Now.AddYears(3) , Dependencies = new List<int>()},
+                    new GanttTask { TaskID = 16, ParentID = 13, TaskName = "Task 3", StartDate = DateTime.Now.AddYears(2), EndDate = DateTime.Now.AddYears(3), BaseStartDate = DateTime.Now.AddYears(3), BaseEndDate = DateTime.Now.AddYears(4) , Dependencies = new List<int>()}
+            };
+        }
+
+        private void btnCalc_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void bbiReset_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form1_Load(this,e);
+        }
+
+        private void bbiClose_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var tasks = GetGanttTasks();
+            ganttControl1.DataSource = tasks;
+            tasks.Find(t => t.TaskID == 3).Dependencies.Add(2); 
+
+        }
+    }
+}
